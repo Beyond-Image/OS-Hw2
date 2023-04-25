7de7a7bbcde7710a032fad19c1a4b33ded349888
Beyond-Image <felipeflores1205@gmail.com>
2023-04-24 22:01:39 -0500

    complete hw

--- client.cpp
+++ client.cpp
@@ -9,13 +9,16 @@
    
 #define PORT     8080
 #define MAXLINE 1024
+
    
 // Driver code
 int main() {
-    int sockfd;
+    int sockfd, serverportnumber;
     char buffer[MAXLINE];
-    const char *hello = "Hello from client";
+    char hello[MAXLINE];
     struct sockaddr_in     servaddr;
+    std::string serverhostname;
+    bool first_time;
    
     // Creating socket file descriptor
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
@@ -24,26 +27,52 @@ int main() {
     }
    
     memset(&servaddr, 0, sizeof(servaddr));
+
+    std::cout << "Enter the server host name: ";
+    std::cin >> serverhostname;
+    std::cout << std::endl;
+
+    std::cout << "Enter the server port number: ";
+    std::cin >> serverportnumber;
+    std::cout << std::endl;
        
     // Filling server information
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(serverportnumber);
+    servaddr.sin_addr.s_addr = INADDR_ANY; //function(127.0.0.1)
        
     int n;
     socklen_t len;
-       
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    std::cout<<"Hello message sent."<<std::endl;
-           
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    std::cout<<"Server :"<<buffer<<std::endl;
+
+    std::cin.getline(hello, MAXLINE);
+    
+    while(true){
+        std::cout << "Enter a car license plate number: ";
+        std::cin.getline(hello, MAXLINE);
+        //std::cout << std::endl;
+
+        if(strcmp(hello, "killsvc") == 0){
+            sendto(sockfd, (const char *)hello, strlen(hello),
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+                    sizeof(servaddr));
+            std::cout << "Client terminates. Bye!" << std::endl;
+            close(sockfd);
+            break;
+        }
+        else{
+            //std::cout << strlen(hello) << std::endl;
+            sendto(sockfd, (const char *)hello, strlen(hello),
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+                    sizeof(servaddr));
+                
+            n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+                        MSG_WAITALL, (struct sockaddr *) &servaddr,
+                        &len);
+            buffer[n] = '\0';
+            std::cout<<buffer<<std::endl;
+        }
+
+    }
    
-    close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,19 +2,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <vector>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <fstream>
+using namespace std;
    
 #define PORT     8080
 #define MAXLINE 1024
+
+
+//port number = 5353
    
 // Driver code
 int main() {
-    int sockfd;
+    int sockfd, serverportnumber;
     char buffer[MAXLINE];
-    const char *hello = "Hello from server";
+    char test[MAXLINE];
+    char hello[MAXLINE];
+    char char_test[MAXLINE];
+    ifstream file_name;
+    string filename, line;
+    vector<string> lines;
+    bool exists;
+
+    cout << "Enter file name: ";
+    cin >> filename;
+    cout << endl;
+    file_name.open(filename);
+    if(!file_name.is_open()){
+        cout << "File is not open!" << endl;
+        return -1;
+    }
+    while(getline(file_name, line)){
+        line.erase(line.size()-1);
+        lines.push_back(line);
+    }
+
+    /*for(int i = 0; i < lines.size(); i++){
+        cout << lines[i] << " " << lines[i].size() << endl;
+        //lines[i].erase(lines[i].size()-1);
+    }*/
+
+    cout << "Enter the server port number: ";
+    cin >> serverportnumber;
+
+
     struct sockaddr_in servaddr, cliaddr;
        
     // Creating socket file descriptor
@@ -29,7 +64,7 @@ int main() {
     // Filling server information
     servaddr.sin_family    = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
+    servaddr.sin_port = htons(serverportnumber);
        
     // Bind the socket with the server address
     if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
@@ -40,19 +75,61 @@ int main() {
     }
        
     socklen_t len;
-  int n;
+    int n;
    
     len = sizeof(cliaddr);  //len is value/result
-   
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-            len);
-    std::cout<<"Hello message sent."<<std::endl; 
+
+    //cout << "len of len: " << len << endl;
+
+    while(true){
+        exists = false;
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                    &len);
+
+
+        buffer[n] = '\0';
+        //printf("Client : %s\n", buffer);
+
+
+        if(strcmp(buffer, "killsvc") == 0){
+            cout << "Recieved request to termainte the service" << endl;
+            cout << "Bye!" << endl;
+            break;
+        }
+        else{
+            for(int i = 0; i < lines.size(); i ++){
+                strcpy(char_test, lines[i].c_str());
+                if(strcmp(buffer, char_test) == 0){
+                    exists = true;
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+            if(exists){
+                string holder = "";
+                cout << buffer << ": Reported as stolen." << endl;
+                holder += buffer;
+                holder += ": Reported as stolen.";
+                strcpy(hello, holder.c_str());
+                sendto(sockfd, (char *)hello, strlen(hello), 
+                    MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+            }
+            else{
+                string holder = "";
+                cout << buffer << ": Not in the database." << endl;
+                holder += buffer;
+                holder += ": Not in the database.";
+                strcpy(hello, holder.c_str());
+                sendto(sockfd, (char *)hello, strlen(hello), 
+                    MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+                        len);
+            }
+        }
+    }
        
     return 0;
 }
